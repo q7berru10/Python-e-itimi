f1a8d49af58d3973d162c9a0fb61a5730822f5d0
q7berru10 <149872383+q7berru10@users.noreply.github.com>
2024-01-28 19:28:35 +0300

    Create agac.py

---
+++ agac.py
@@ -0,0 +1,7 @@
+print("...PYTHON EĞTİMLERİ...")
+for i in range(1, 10):
+    print(' ' * (9 - i) +  '*' * (2 * i - 1))
+for m in range(0,5):
+  for j in range(4,5):
+        print(" ", end=" ")
+        print("   *******")
