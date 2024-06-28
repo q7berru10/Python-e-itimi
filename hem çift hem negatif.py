e650aa00b74d3394e79a515cae56ae0b34d81ff0
q7berru10 <149872383+q7berru10@users.noreply.github.com>
2024-06-28 21:17:23 +0300

    Add files via upload

---
+++ hem çift hem negatif.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 28 21:14:49 2024
+
+@author: tusem
+"""
+
+sayii= int(input("lütfen bbir sayı giriniz:"))
+if (sayii>0) and (sayii%2==0) :
+    print("sayınız pozitiftir ve çiftir")
+else:
+    print("sayınız tek  negatif ")
---
+++ sayı büyük ve küçük elif-if.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 28 20:27:27 2024
+
+@author: tusem
+"""
+"""
+a=int(input("bir sayı giriniz:"))
+
+if a<100:
+    print("girdiğiniz sayı100den küçüktür")
+elif a<50:
+    print("verdiğiniz sayı 50den güçüktür")
+elif a==100:
+    print("verdiğiniz sayı100e eşittir")
+elif a>150:
+    print("verdiğiniz sayı150den büyüktür")
+elif a>100:
+    print("girdiğiniz sayı 100den büyüktür")
+    
+#if vkendinden sonraki dopğrulari çalıştıtırkrn elif doğruyu bulduğunda dururu (kodlar yukardan aşşağı okunur elifde dur der)
+    
+a=int(input("bir sayı giriniz:"))
+
+if a<100:
+    print("girdiğiniz sayı100den küçüktür")
+if a<50:
+    print("verdiğiniz sayı 50den güçüktür")
+if a==100:
+    print("verdiğiniz sayı100e eşittir")
+if a>150:
+    print("verdiğiniz sayı150den büyüktür")s
+if a>100:
+    print("girdiğiniz sayı 100den büyüktür")
+
+        
+a=int(input("bir sayı giriniz:"))
+
+if   a>50 and a<100:
+    print("50den büyük yüzden küçük")
+if a<50 and a>0:
+    print("50dende küçük 0dean büyük")
+if a>50 and a==100:
+    print("50den büyük 100 eşit")
+if a==50 and a<100:
+    print("50ye eşit 100den küçük")
+
+        """
+    
+    
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
