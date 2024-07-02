abbc8b4d938a47a96aee72b277d3ed3f361265f1
q7berru10 <149872383+q7berru10@users.noreply.github.com>
2024-07-02 18:47:43 +0300

    Add files via upload

---
+++ email  parola sisteme giriş.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 28 21:20:54 2024
+
+@author: tusem
+"""
+
+kullanıcı_adi=input("Lütfen Kullanıcı Adınızı Giriniz= ")
+sifre=input("Lütfen Şifrenizi Giriniz= ")
+
+if(kullanıcı_adi=="berru_10B") :
+   if (sifre=="1007"):
+       print("Hoşgeldiniz....")
+   else:
+           print(" hatalı!  şifre ")
+else:
+    print(" hatalı! kullanıcı adı  ")
